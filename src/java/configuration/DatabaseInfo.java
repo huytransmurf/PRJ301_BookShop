8be92f4b5c56a73e05970f4b52ef09351f5840e0
quangks123 <quang123ks@gmail.com>
2024-07-14 21:54:11 +0700

    profile no design

--- src/java/configuration/DatabaseInfo.java
+++ src/java/configuration/DatabaseInfo.java
@@ -20,9 +20,9 @@ public interface DatabaseInfo {
 //    public static String USERDB="sa";
 //    public static String PASSDB="123";
 //    // Cua t de day ko dung comment lai
-    public static String DBURL="jdbc:sqlserver://LAPTOP-7M53ESA2;databaseName=FruitStore;encrypt=false;trustServerCertificate=false;loginTimeout=30;";
-    public static String USERDB="sa";    
-    public static String PASSDB="zxcl123123";
+//    public static String DBURL="jdbc:sqlserver://LAPTOP-7M53ESA2;databaseName=FruitStore;encrypt=false;trustServerCertificate=false;loginTimeout=30;";
+//    public static String USERDB="sa";    
+//    public static String PASSDB="zxcl123123";
 //  
 ////    public static String DBURL="jdbc:sqlserver://LAPTOP-7M53ESA2;databaseName=FruitStore;encrypt=false;trustServerCertificate=false;loginTimeout=30;";
 ////    public static String USERDB="sa";    
